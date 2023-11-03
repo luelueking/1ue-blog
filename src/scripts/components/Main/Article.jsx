f268c2e59b2bcdfad8fddd3cb8c8314ad5295576
陈再 <2336485988@qq.com>
2023-11-03 10:14:05 +0800

    fix article

--- src/scripts/components/Main/Article.jsx
+++ src/scripts/components/Main/Article.jsx
@@ -2,35 +2,37 @@ import React from 'react'
 import member_info from '../../config/member.js'
 
 export default (props) => {
-    const memberProfile = member_info.map( member =>
+    const memberProfile = member_info.map(member =>
         <div className="profile" key={member.id}>
             <a className="member__avatar"
                href={member.url}
                title={member.id}
                target="_blank"
             >
-                <img title={member.id} src={member.avatar} />
+                <img title={member.id} src={member.avatar}/>
                 <span>{member.id}</span>
             </a>
-            <div className="member__intro">
-                <p
-                    dangerouslySetInnerHTML={{
-                        __html:
-                        member.intro
-                    }}
-                />
-            </div>
+
+                <div className="member__intro">
+                    <p
+                        onClick={() => { window.location.href = member.url; }}
+                        dangerouslySetInnerHTML={{
+                            __html:
+                            member.intro
+                        }}
+                    />
+                </div>
         </div>
     )
 
     return (
         <section className="_member">
-            <div className="member__line-start" />
+            <div className="member__line-start"/>
             <div className="member__content">
                 <h2>Articles</h2>
                 {memberProfile}
             </div>
-            <div className="member__line-end" />
+            <div className="member__line-end"/>
         </section>
     )
 }
