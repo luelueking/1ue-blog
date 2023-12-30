c20835367ee8acf512810cd77a7e73d51303ab43
陈再 <2336485988@qq.com>
2023-12-30 16:53:38 +0800

    fix date

--- src/scripts/config/member.js
+++ src/scripts/config/member.js
@@ -12,7 +12,7 @@ export default [
     url: "https://vidar-team.feishu.cn/docx/Vy94dHrmDo2zLyx79qXcSbMHnwc?from=from_copylink"
   },
   {
-    id: "2023.09.21",
+    id: "2023.08.30",
     intro: "JNDI JDK高版本绕过--Hikari",
     avatar: require("../../images/avatar/feishu.png"),
     url: "https://vidar-team.feishu.cn/docx/PL7IdIkFCoBYhYxxFXVc5j72nKo?from=from_copylink"
